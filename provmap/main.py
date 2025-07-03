f6fe52d8d096ecd4975dae241fb01677ede3bbeb
Egg-Fish <eugeniomanansala@gmail.com>
2025-07-03 15:44:44 +0800

    feat: main

--- provmap/main.py
+++ provmap/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from functools import reduce
 
 
@@ -6,6 +7,7 @@ from provmap.embedder import Embedder
 from provmap.loader import Loader
 from provmap.reasoner import Reasoner
 from provmap.graph.graph import Graph
+import argparse
 
 
 logger = logging.getLogger(__name__)
@@ -15,6 +17,7 @@ logger.debug("Application start")
 CONFIG_APT17 = {
     "name": "ProvCon APT17 Scenario",
     "dir": "examples/APT17",
+    "outdir": "out/APT17",
     "logs": {
         "sysmon": [
             # "Sysmon.evtx",
@@ -29,6 +32,7 @@ CONFIG_APT17 = {
 CONFIG_T1059_005 = {
     "name": "Splunk T1059.005 Scenario",
     "dir": "examples/T1059.005",
+    "outdir": "out/T1059.005",
     "logs": {
         "sysmon": [
             "Sysmon.txt",
@@ -39,6 +43,7 @@ CONFIG_T1059_005 = {
 CONFIG_APT32 = {
     "name": "ProvCon APT32-B Scenario",
     "dir": "examples/APT32",
+    "outdir": "out/APT32",
     "logs": {
         "sysmon": [
             "sysmon/victim_1_3_11.evtx",
@@ -46,9 +51,29 @@ CONFIG_APT32 = {
     },
 }
 
+CONFIG_APT33 = {
+    "name": "ProvCon APT33 Scenario",
+    "dir": "examples/APT33",
+    "outdir": "out/APT33",
+    "logs": {
+        "sysmon": [
+            # "sysmon/aviation.evtx",
+            # "sysmon/energy.evtx",
+            "sysmon/aviation_1_3_11.evtx",
+            "sysmon/energy_1_3_11.evtx",
+        ],
+        "pcap": [
+            "pcap/APT33_combined.pcap",
+            # "pcap/EnergySectorServer_combined.pcap",
+            # "pcap/AviationSectorServer_combined.pcap",
+        ],
+    },
+}
+
 CONFIG_SHARPHOUND = {
     "name": "Splunk T1059.001 SharpHound Scenario",
     "dir": "examples/SharpHound",
+    "outdir": "out/SharpHound",
     "logs": {
         "sysmon": [
             "windows-sysmon.txt",
@@ -56,8 +81,23 @@ CONFIG_SHARPHOUND = {
     },
 }
 
+CONFIG_MIXTURE = {
+    "name": "A collection of various Sysmon logs",
+    "dir": "examples",
+    "outdir": "out/mixture",
+    "logs": {
+        "sysmon": [
+            "APT17/Sysmon.evtx",
+            "APT32/sysmon/victim.evtx",
+            "EvtxAttackSamples/Exec_sysmon_meterpreter_reversetcp_msipackage.evtx",
+            "SharpHound/windows-sysmon.txt",
+            "T1059.005/Sysmon.txt",
+        ]
+    },
+}
 
-def save_as_graphviz(graph: Graph, outpath: str):
+
+def save_graph_as_graphviz(graph: Graph, outpath: str):
     logger.info(f"Saving graph {graph} as Graphviz file {outpath}")
 
     gv: str = graph.to_graphviz()
@@ -66,7 +106,7 @@ def save_as_graphviz(graph: Graph, outpath: str):
         f.write(gv)
 
 
-def save_as_prolog(graph: Graph, outpath: str):
+def save_graph_as_prolog(graph: Graph, outpath: str):
     logger.info(f"Saving graph {graph} as Prolog file {outpath}")
 
     prolog: str = graph.to_prolog()
@@ -75,7 +115,7 @@ def save_as_prolog(graph: Graph, outpath: str):
         f.write(prolog)
 
 
-def save_as_triples(graph: Graph, outpath: str):
+def save_graph_as_triples(graph: Graph, outpath: str):
     logger.info(f"Saving graph {graph} as triples {outpath}")
 
     triples: list = graph.to_triples()
@@ -85,57 +125,209 @@ def save_as_triples(graph: Graph, outpath: str):
             f.write("\t".join(map(str, t)) + "\n")
 
 
+def save_graph_as_pickle(graph: Graph, outpath: str):
+    logger.info(f"Saving graph {graph} as pickle {outpath}")
+
+    pkl: bytes = graph.to_pickle()
+
+    with open(outpath, "wb") as f:
+        f.write(pkl)
+
+
+def load_graph_from_pickle(inpath: str) -> Graph:
+    logger.info(f"Loading graph from pickle {inpath}")
+
+    with open(inpath, "rb") as f:
+        pkl = f.read()
+
+    return Graph.from_pickle(pkl)
+
+
+def save_embedder_as_pickle(embedder: Embedder, outpath: str):
+    logger.info(f"Saving embedder as pickle {outpath}")
+
+    pkl: bytes = embedder.to_pickle()
+
+    with open(outpath, "wb") as f:
+        f.write(pkl)
+
+
+def load_embedder_from_pickle(inpath: str) -> Embedder:
+    logger.info(f"Loading embedder from pickle {inpath}")
+
+    with open(inpath, "rb") as f:
+        pkl = f.read()
+
+    return Embedder.from_pickle(pkl)
+
+
+def load_graph(config: dict, force_rebuild: bool = False, include_pcap: bool = False):
+    loader = Loader(config)
+
+    outdir = config["outdir"]
+
+    try:
+        assert force_rebuild == False
+        pickle_inpath = os.path.join(outdir, "graph.pkl")
+        graph = load_graph_from_pickle(pickle_inpath)
+        logger.info(f"Loaded existing graph from {pickle_inpath}")
+
+    except:
+        logger.info("Constructing graph from scratch")
+
+        graph: Graph = loader.construct_graph(include_pcap=include_pcap)
+
+        outdir = config["outdir"]
+        os.makedirs(outdir, exist_ok=True)
+
+        save_graph_as_graphviz(graph, os.path.join(outdir, "graph.gv"))
+        save_graph_as_prolog(graph, os.path.join(outdir, "graph.pl"))
+        save_graph_as_triples(graph, os.path.join(outdir, "graph.txt"))
+
+        save_graph_as_pickle(graph, os.path.join(outdir, "graph.pkl"))
+
+    return graph
+
+
+def load_embedder(
+    config: dict, graph: Graph, reasoner: Reasoner | None, force_retrain=False
+) -> Embedder:
+    outdir = config["outdir"]
+
+    try:
+        assert force_retrain == False
+        embedder_inpath = os.path.join(outdir, "embedder.pkl")
+        embedder = load_embedder_from_pickle(embedder_inpath)
+
+    except:
+        logger.info("Could not find existing embedder. Training embedder from scratch")
+
+        embedder = Embedder(graph, reasoner=reasoner)
+
+        embedder.train(embedding_dim=32, num_epochs=3000)
+
+        logger.info(f"Metrics: {embedder.metrics}")
+
+        save_embedder_as_pickle(embedder, os.path.join(outdir, "embedder.pkl"))
+
+    return embedder
+
+
+PROVCON_DATASET_DIR = os.getenv("PROVCON_DATASET_DIR")
+
+
+def load_from_provcon(scenario: str, date: str, time: str) -> dict:
+    dir = f"{PROVCON_DATASET_DIR}/{scenario}/{date}/{time}"
+    outdir = f"./out/{scenario}/{date}/{time}"
+
+    os.makedirs(outdir, exist_ok=True)
+
+    os.makedirs(outdir + "/sysmon", exist_ok=True)
+    os.makedirs(outdir + "/pcap", exist_ok=True)
+
+    sysmon_zips = os.listdir(dir + "/logs")
+
+    sysmon_zips = [s for s in sysmon_zips if "_events_" in s]
+
+    sysmon_files = []
+    pcap_files = []
+
+    for z in sysmon_zips:
+        instance_name = z.split("_")[0]
+
+        zip_path = f"{dir}/logs/{z}"
+        sysmon_file = f"sysmon/{instance_name}.evtx"
+
+        os.system(f'unzip -o {zip_path} "*Sysmon*.evtx" -d {outdir}/sysmon')
+        os.system(f"mv {outdir}/sysmon/**/*Sysmon*.evtx {outdir}/{sysmon_file}")
+
+        sysmon_files.append(sysmon_file)
+
+    for p in [p for p in os.listdir(dir + "/network") if p.endswith(".pcap")]:
+        instance_name = p.split("_")[0]
+
+        pcap_path = dir + "/network/" + p
+        pcap_file = f"pcap/{instance_name}.pcap"
+
+        os.system(f"cp {pcap_path} {outdir}/{pcap_file}")
+
+        pcap_files.append(pcap_file)
+
+    return {
+        "name": f"PROVCON {scenario} {date} {time}",
+        "dir": outdir,
+        "outdir": outdir,
+        "logs": {
+            "sysmon": sysmon_files,
+            "pcap": pcap_files,
+        },
+    }
+
+
 def main():
-    logger.info("Main started")
-
-    loader = Loader(CONFIG_APT17)
-    graph: Graph = loader.construct_graph()
-
-    save_as_graphviz(graph, "out/graph.gv")
-    save_as_prolog(graph, "out/graph.pl")
-    save_as_triples(graph, "out/graph.txt")
-
-    embedder = Embedder(graph)
-    embedder.train(embedding_dim=80, num_epochs=400)
-
-    # score = embedder.score_hrt(
-    #     "file_021b6e05bb60ccb34383fd3c7e3c09170cbf88142ca0361ba63300a61f09290e",
-    #     "connects_to",
-    #     "172.202.163.200_443",
-    # )
-
-    scores = embedder.score_t(
-        "{65861cbf-c6ba-682f-7202-000000000600}",
-        "connects_to",
-        sort=True
+    parser = argparse.ArgumentParser(description="provmap")
+    parser.add_argument(
+        "--scenario", type=str, required=True, help="Scenario name (e.g. APT33)"
+    )
+    parser.add_argument("--date", type=str, required=True, help="Date (YYYY-MM-DD)")
+    parser.add_argument("--time", type=str, required=True, help="Time (HHMM)")
+    parser.add_argument(
+        "--force-rebuild", action="store_true", help="Force rebuild of the graph"
+    )
+    parser.add_argument(
+        "--include-pcap", action="store_true", help="Include packet captures in graph"
     )
 
-    # scores = embedder.score_t(
-    #     "192.168.56.100_21",
-    #     "responds_to",
-    #     sort=True
-    # )
+    args = parser.parse_args()
 
-    for row in scores[:10]:
-        i, score = row
-        print(f"Entity: {embedder.entities[int(i)]}, Score: {score}")
+    logger.info("Main started")
 
-    embedder.plot("out/entity_embeddings.html")
+    config = load_from_provcon(args.scenario, args.date, args.time)
+    outdir = config["outdir"]
+    graph = load_graph(
+        config,
+        force_rebuild=args.force_rebuild,
+        include_pcap=args.include_pcap,
+    )
 
     reasoner = Reasoner(graph, "rules/schema.pl", "rules/rules.pl")
 
     malicious_entities = reasoner.get_malicious_entities()
+    malicious_graph = graph.subgraph(malicious_entities)
 
-    for e in malicious_entities:
-        tags = reasoner.get_tags(e)
+    save_graph_as_graphviz(malicious_graph, os.path.join(outdir, "malicious_graph.gv"))
 
-        embedding = embedder.get_entity_embedding(e.entity_id)
+    for entity_id in malicious_graph.G.nodes():
+        tags = reasoner.get_tags(malicious_graph.get_entity(entity_id))
+        logger.info(f"Tags for {entity_id}: \n{"\n".join(tags)}")
 
-        logger.info(f"Malicious entity {e} with tags {tags}")
+    exit()
 
-    malicious_traces = [graph.trace(e.entity_id) for e in malicious_entities]
-    malicious_traces += [Graph(), Graph()]  # Prevent reduce() from crashing
+    # terminals = malicious_graph.get_leaves()
 
-    malicious_graph = reduce(lambda acc, x: acc.combine(x), malicious_traces)
+    # for terminal in terminals:
+    #     trace = malicious_graph.trace(terminal)
 
-    save_as_graphviz(malicious_graph, "out/malicious_graph.gv")
+    #     walks = trace.to_walks(label=True)
+
+    #     for walk in walks:
+    #         print(" ".join(f"[{n}]" for n in walk))
+
+    # exit()
+
+    # embedder = load_embedder(config, graph, reasoner, force_retrain=True)
+
+    # embedder.plot(os.path.join(outdir, "entity_embedding.html"))
+    # embedder.to_csv(
+    #     os.path.join(outdir, "entity_embeddings.csv"),
+    #     os.path.join(outdir, "relation_embeddings.csv"),
+    # )
+
+    # # Tail prediction
+
+    # entity_id = "file_42e889d3558c5e0efa1508784f3e2e15f5b9b6256075d4d3cfdb062e6478c36a"
+    # scores = embedder.score_t(entity_id, "has_tag", sort=True)
+
+    # for row in scores[:10]:
+    #     i, score = row
+    #     print(f"Entity: {embedder.entities[int(i)]}, Score: {score}")
